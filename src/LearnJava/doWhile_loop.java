c077f31af1be7f9dbd2eff70331c8e0583502d5d
priya <patanepriyanka@gmail.com>
2026-01-11 12:24:46 +0530

    push to git

---
+++ src/LearnJava/doWhile_loop.java
@@ -0,0 +1,16 @@
+package LearnJava;
+
+public class doWhile_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long i = 244223l;
+		
+		do {
+			System.out.println("Dance" + i);
+			i++; i++;
+			
+			} while(i<244226);
+	}
+
+}
---
+++ src/LearnJava/while_loop_PP.java
@@ -0,0 +1,13 @@
+package LearnJava;
+
+public class while_loop_PP {
+	public static void main(String[] arg) {
+		double i = 244220.9;
+
+		while (i < 244224.8) {
+			System.out.println("Dance" + i);
+			i++;
+			i++;
+		}
+	}
+}
